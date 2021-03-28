ac228a9465d953a3bda94d9c60648e534dd7a864
Jack Zhang <jzhanglsw@gmail.com>
2021-03-27 22:25:29 -0400

    Changed port due to flask api and react front end config

--- app.js
+++ app.js
@@ -10,6 +10,6 @@ app.use("/graphql", graphqlHTTP({
 }));
 
 
-app.listen(3000, () => {
-    console.log("Listening at :3000...");
+app.listen(4000, () => {
+    console.log("Listening at :4000...");
 });
